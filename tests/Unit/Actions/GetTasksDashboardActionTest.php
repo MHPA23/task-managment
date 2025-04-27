24332c7f02d091f6f9a0bd452aac666b1c3accb6
Matheus Affonso <matheuspradoaffonso@gmail.com>
2025-04-27 10:29:46 -0300

    aadjustment test to make sure that return categoryDistribuition

--- tests/Unit/Actions/GetTasksDashboardActionTest.php
+++ tests/Unit/Actions/GetTasksDashboardActionTest.php
@@ -1,21 +1,33 @@
 <?php
 
 use App\Actions\GetTasksDashboardAction;
+use App\Models\Category;
 use App\Models\Task;
 
 it('should return tasks values from dashboard', function () {
     Task::factory()->count(5)->create([
         'completed' => true,
         'due_date' => now()->addDays(1),
+        'category_id' => Category::factory()->create([
+            'name' => 'feature',
+        ]),
     ]);
 
     Task::factory()->count(3)->create([
         'completed' => false,
         'due_date' => now()->addDays(1),
+        'category_id' => Category::factory()->create([
+            'name' => 'bug',
+        ]),
     ]);
     $action = new GetTasksDashboardAction;
     $result = $action->handle();
 
     expect($result['completedTasks'])->toEqual(5);
     expect($result['pendingTasks'])->toEqual(3);
+    expect(collect($result['categoryDistribution']->toArray())->pluck('count', 'category')->all())
+        ->toEqual([
+            'feature' => 5,
+            'bug' => 3,
+        ]);
 });
